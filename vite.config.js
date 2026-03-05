e40095396736fdce44b326f44eb2beeb9fba6f23
Israel Merlyn <merlyn142a@gmail.com>
2026-03-05 09:15:11 -0600

    tasks #2 Definiciendo Conf para  PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'NASA Asteroids Dashboard',
+        short_name: 'NeoWs Dash',
+        description: 'Monitoreo de asteroides cercanos a la Tierra',
+        theme_color: '#0b3d91', 
+        icons: [
+          {
+            src: '/pwa-192x192.png', 
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
